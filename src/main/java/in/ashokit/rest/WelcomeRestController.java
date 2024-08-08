d6b2fa42f1b1f9c807c13adad15d993d359a4976
Bhushan <garadbhushan1@gmail.com>
2024-08-08 14:07:28 +0530

    logger added

--- src/main/java/in/ashokit/rest/WelcomeRestController.java
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -53,6 +53,7 @@ public class WelcomeRestController {
 		if(!change){
 			int a = 25;
 			int b = 35;
+			logger.info("*************************************");
 			String str = "garad bhushan";
 			logger.info("**** fetch command changes not added****");
 		}else{
