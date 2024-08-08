5654fd6c76151b9d0d046136e9b190c864bc062a
Bhushan <garadbhushan1@gmail.com>
2024-08-08 13:17:31 +0530

    fetch changes removed

--- src/main/java/in/ashokit/rest/WelcomeRestController.java
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -47,16 +47,8 @@ public class WelcomeRestController {
 	//task 2
 	public void m2(){
 		String str = "task 2 related changes";
-		String errorMsg = "fetch command";
-		logger.info("****** Conflict code added here to check fetch command ******");
 		boolean change = false;
 		//logic
 	}
-
-	//New changes to test fetch command
-	public void m3(){
-		int i = 10;
-		int j = 20;
-		//logic to add values
-	}
+	
 }
